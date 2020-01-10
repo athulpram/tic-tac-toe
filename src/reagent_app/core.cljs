99c5da28815dfeaef2959e1a5f7207f1492de11f
praathul <athul.p.aux@go-jek.com>
2020-01-10 21:38:51 +0530

    Modified core to change player after each turn

--- src/reagent_app/core.cljs
+++ src/reagent_app/core.cljs
@@ -1,40 +1,41 @@
 (ns reagent-app.core
     (:require
       [reagent.core :as r]))
-    ;(:require-macros
-    ;  [cljss.core :refer [defstyles]])
 
-;; -------------------------
-;; Views
-;
-;(defstyles cell-style []
-;           {
-;            :width 10
-;            :height 10
-;            })
+(def table-data (r/atom {:one nil, :two nil, :three nil, :four nil, :five nil, :six nil, :seven nil, :eight nil, :nine nil}))
+(def players (r/atom ["O", "X"]))
+(def current-player (r/atom 0))
 
-(defn table [table-data]
+(defn change-turn []
+  (swap! current-player (fn [n] (- (- n 1)))))
+
+(defn mark [event]
+  (do
+    (swap! table-data assoc-in [(keyword event.target.id)] (@players @current-player))
+    (change-turn)))
+
+(defn tic-tac-toe-table [table-data]
   [:div [:table
          [:tr
-          [:td [:button {:style {:width 100 :height 100 :font-size 50}} (:one table-data)]]
-          [:td [:button {:style {:width 100 :height 100 :font-size 50}} (:two table-data)]]
-          [:td [:button {:style {:width 100 :height 100 :font-size 50}} (:three table-data)]]
+          [:td [:button {:id :one :style {:width 100 :height 100 :font-size 50} :on-click mark} (:one @table-data)]]
+          [:td [:button {:id :two :style {:width 100 :height 100 :font-size 50} :on-click mark} (:two @table-data)]]
+          [:td [:button {:id :three :style {:width 100 :height 100 :font-size 50} :on-click mark} (:three @table-data)]]
           ]
          [:tr
-          [:td [:button {:style {:width 100 :height 100 :font-size 50}} (:four table-data)]]
-          [:td [:button {:style {:width 100 :height 100 :font-size 50}} (:five table-data)]]
-          [:td [:button {:style {:width 100 :height 100 :font-size 50}} (:six table-data)]]]
+          [:td [:button {:id :four :style {:width 100 :height 100 :font-size 50} :on-click mark} (:four @table-data)]]
+          [:td [:button {:id :five :style {:width 100 :height 100 :font-size 50} :on-click mark} (:five @table-data)]]
+          [:td [:button {:id :six :style {:width 100 :height 100 :font-size 50} :on-click mark} (:six @table-data)]]]
          [:tr
-          [:td [:button {:style {:width 100 :height 100 :font-size 50}} (:seven table-data)]]
-          [:td [:button {:style {:width 100 :height 100 :font-size 50}} (:eight table-data)]]
-          [:td [:button {:style {:width 100 :height 100 :font-size 50}} (:nine table-data)]]
+          [:td [:button {:id :seven :style {:width 100 :height 100 :font-size 50} :on-click mark} (:seven @table-data)]]
+          [:td [:button {:id :eight :style {:width 100 :height 100 :font-size 50} :on-click mark} (:eight @table-data)]]
+          [:td [:button {:id :nine :style {:width 100 :height 100 :font-size 50} :on-click mark} (:nine @table-data)]]
           ]]])
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (r/render [table {:one 0, :two 0, :three 1, :four 1, :five 1, :six 0, :seven 0, :eight 1, :nine 1}] (.getElementById js/document "app")))
+  (r/render [tic-tac-toe-table table-data] (.getElementById js/document "app")))
 
 (defn init! []
   (mount-root))
